5c517b1a4fca0ae39b83c6162cde27c129abb039
Hotpause <ashutoshagarwal1006@gmail.com>
2024-07-18 11:24:01 +0530

    Add job add and delete function

--- src/pages/AddJobPage.jsx
+++ src/pages/AddJobPage.jsx
@@ -1,6 +1,7 @@
 import { React, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-const AddJobPage = () => {
+const AddJobPage = ({ addJobSubmit }) => {
   const [title, setTitle] = useState("");
   const [type, setType] = useState("Full-Time");
   const [location, setLocation] = useState("");
@@ -10,6 +11,7 @@ const AddJobPage = () => {
   const [companyDescription, setCompanyDescription] = useState("");
   const [contactEmail, setContactEmail] = useState("");
   const [contactPhone, setContactPhone] = useState("");
+  const navigate = useNavigate();
 
   const submitForm = (e) => {
     e.preventDefault();
@@ -26,7 +28,8 @@ const AddJobPage = () => {
         contactPhone,
       },
     };
-    console.log(newJob);
+    addJobSubmit(newJob);
+    return navigate("/jobs");
   };
 
   return (
