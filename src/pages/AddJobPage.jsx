dc0bf0dd93d11b1c0ee4d9c2fa7cce66337ffc06
Hotpause <ashutoshagarwal1006@gmail.com>
2024-07-19 13:35:24 +0530

    Add UI change

--- src/pages/AddJobPage.jsx
+++ src/pages/AddJobPage.jsx
@@ -35,7 +35,7 @@ const AddJobPage = ({ addJobSubmit }) => {
   };
 
   return (
-    <section className="bg-teal-50">
+    <section className="bg-indigo-50">
       <div className="container m-auto max-w-2xl py-24">
         <div className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
           <form onSubmit={submitForm}>
@@ -217,7 +217,7 @@ const AddJobPage = ({ addJobSubmit }) => {
 
             <div>
               <button
-                className="bg-teal-500 hover:bg-teal-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline"
+                className="bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline"
                 type="submit"
               >
                 Add Job
--- src/pages/EditJobPage.jsx
+++ src/pages/EditJobPage.jsx
@@ -40,7 +40,7 @@ const EditJobPage = ({ updateJobSubmit }) => {
     return navigate(`/jobs/${id}`);
   };
   return (
-    <section className="bg-teal-50">
+    <section className="bg-indigo-50">
       <div className="container m-auto max-w-2xl py-24">
         <div className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
           <form onSubmit={submitForm}>
@@ -224,7 +224,7 @@ const EditJobPage = ({ updateJobSubmit }) => {
 
             <div>
               <button
-                className="bg-teal-500 hover:bg-teal-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline"
+                className="bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline"
                 type="submit"
               >
                 Update Job
