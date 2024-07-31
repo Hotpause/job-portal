57debfc1a53bad8e5d5ddde5809f3d6ee75337ca
Hotpause <ashutoshagarwal1006@gmail.com>
2024-07-31 21:24:04 +0530

    Add dummy data

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -3,25 +3,32 @@ import Spinner from "./Spinner";
 import JobListing from "./JobListing";
 
 const JobListings = ({ isHome = false }) => {
-  //   console.log(jobs);
   const [jobs, setJobs] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchJobs = async () => {
-      const apiUrl = isHome ? "/api/jobs?_limit=3" : "/api/jobs";
+      const apiUrl = isHome ? "/jobs.json" : "/jobs.json"; // Adjust URL based on isHome
       try {
         const res = await fetch(apiUrl);
         const data = await res.json();
-        setJobs(data);
+        if (data.jobs && Array.isArray(data.jobs)) {
+          setJobs(data.jobs);
+        } else {
+          console.error(
+            "Fetched data does not contain a valid 'jobs' array:",
+            data
+          );
+        }
       } catch (error) {
-        console.log("error fetching data", error);
+        console.log("Error fetching data:", error);
       } finally {
         setLoading(false);
       }
     };
     fetchJobs();
-  }, []);
+  }, [isHome]);
+
   return (
     <section className="bg-indigo-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
@@ -34,7 +41,7 @@ const JobListings = ({ isHome = false }) => {
         ) : (
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
             {jobs.map((job) => (
-              <JobListing key={job.id} job={job}></JobListing>
+              <JobListing key={job.id} job={job} />
             ))}
           </div>
         )}
